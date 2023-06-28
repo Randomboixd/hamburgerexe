b781b37edb6cc543b273ec5abb4ab36ce8015208
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2023-06-28 08:14:34 +0200

    Change font so no funny moment :( also added way to store records, and oh yes .gitignore

--- game.py
+++ game.py
@@ -1,25 +1,60 @@
 import pygame
 import os
 import random
+import base64
+import json
+import requests
 
 
 pygame.init()
 os.system("cls")
 
+
+def DownloadFiles():
+    if os.path.exists("./HamburgerAssets/") and os.path.exists(f"./HamburgerAssets/burgir.png") and os.path.exists("./HamburgerAssets/cheeseburger.png") and os.path.exists("./HamburgerAssets/Player.png") and os.path.exists("./HamburgerAssets/spike.png") and os.path.exists("./HamburgerAssets/toilet.png"):
+        print("Game assets are present, Starting game!")
+    else:
+        os.mkdir("./HamburgerAssets/")
+        print("Connecting to https://rndwebsite.ddns.net/ and downloading assets!")
+        burgir = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/burgir")
+        cheeseburger = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/cheeseburger")
+        Player = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/Player")
+        Spike = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/spike")
+        toilet = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/toilet")
+
+        with open(f"./HamburgerAssets/burgir.png", "xb") as f:
+            f.write(burgir.content)
+
+        with open(f"./HamburgerAssets/cheeseburger.png", "xb") as f:
+            f.write(cheeseburger.content)
+
+        with open(f"./HamburgerAssets/Player.png", "xb") as f:
+            f.write(Player.content)
+        
+        with open(f"./HamburgerAssets/spike.png", "xb") as f:
+            f.write(Spike.content)
+        
+        with open(f"./HamburgerAssets/toilet.png", "xb") as f:
+            f.write(toilet.content)
+
+        print("Finished downloading (and writing) files! Starting game!")
+
+DownloadFiles()
+
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Hamburger.exe")
-spikeSprite = pygame.image.load("spike.png")
-logo = pygame.image.load("burgir.png")
-toiletSprite = pygame.image.load("toilet.png")
-cheseburgerSprite = pygame.image.load("cheeseburger.png")
+pygame.display.set_caption("./HamburgerAssets/Hamburger.exe")
+spikeSprite = pygame.image.load("./HamburgerAssets/spike.png")
+logo = pygame.image.load("./HamburgerAssets/burgir.png")
+toiletSprite = pygame.image.load("./HamburgerAssets/toilet.png")
+cheseburgerSprite = pygame.image.load("./HamburgerAssets/cheeseburger.png")
 pygame.display.set_icon(logo)
 del logo
-GFont = pygame.font.SysFont("Comic Sans MS", 30)
-GFont_smaller = pygame.font.SysFont("Comic Sans MS", 20)
-GFont_large = pygame.font.SysFont("Comic Sans MS", 40)
+GFont = pygame.font.SysFont("Cascadia Code SemiBold", 40)
+GFont_smaller = pygame.font.SysFont("Cascadia Code", 30)
+GFont_large = pygame.font.SysFont("Cascadia Mono SemiBold", 60)
 fontcenter = (200 , 20)
 fontbottom = (250, 500)
-burgerstat = (100, 10)
+burgerstat = (1, 40)
 colorcodes = [(102,17,0), (128,64,0), (102,102,0), (85,128,0), (42,128,0), (0,128,64), (0,128,128), (0,85,128), (42,0,128), (85,0,128), (128,0,106)]
 backroundcolor = random.choice(colorcodes)
 
@@ -30,6 +65,7 @@ PlayerIcon = pygame.image.load("Player.png")
 PlayerX,PlayerY = 400, 300
 MovingConstantX = 0
 MovingConstantY = 0
+controlsInverted = False
 
 cheseburgerloc = (random.randint(200, 800), random.randint(100, 600))
 cheseburgerstouched = 0
@@ -49,6 +85,16 @@ ToiletMovingConstantY = 0
 GameOver = False
 
 reaction = GFont_smaller.render("Event: - nothing -", False, (255,255,255))
+
+if os.path.exists("hamburger.records"):
+    with open("hamburger.records", "r") as f:
+        f = json.load(f)
+    record = int(base64.b64decode(f["highscore"].encode()).decode())
+    del f
+else:
+    record = 0
+
+Saved = False
 
 spikes = []
 
@@ -78,9 +124,31 @@ def tutorialscreen():
         a = GFont_smaller.render("Or death will be your only choice! now GO!", False, (255,255,255))
         screen.blit(a, fontcenter)
 
-def GameOverScreen():
-    gameoverscreentitle = GFont_large.render("Game over.", False, (255,0,0))
-    screen.blit(gameoverscreentitle, (30, 300))
+def GameOverScreen(Saved):
+    
+    if Saved != True:
+        if os.path.exists("./hamburger.records"):
+            
+            with open("hamburger.records", "r") as f:
+                f = json.load(f)
+
+            highscore = int(base64.b64decode(f["highscore"].encode()).decode())
+
+            if highscore < cheseburgerstouched:
+                with open("hamburger.records", "w") as f:
+                    record = base64.b64encode(str(cheseburgerstouched).encode()).decode()
+                    json.dump({"highscore": record}, f)
+            
+        else:
+            with open("hamburger.records", "x") as f:
+                highscore = base64.b64encode(str(cheseburgerstouched).encode()).decode()
+                json.dump({"highscore": highscore}, f)
+        return True
+
+    
+            
+
+
 
 def renderSpikes():
     s = []
@@ -101,16 +169,28 @@ while runs:
         if events.type == pygame.KEYDOWN:
             
             if events.key == pygame.K_w or events.key == pygame.K_UP:
-                MovingConstantY -= moveSpeed
+                if controlsInverted:
+                    MovingConstantY += moveSpeed
+                else:
+                    MovingConstantY -= moveSpeed
             
             if events.key == pygame.K_s or events.key == pygame.K_DOWN:
-                MovingConstantY += moveSpeed
+                if controlsInverted:
+                    MovingConstantY -= moveSpeed
+                else:
+                    MovingConstantY += moveSpeed
 
             if events.key == pygame.K_d or events.key == pygame.K_RIGHT:
-                MovingConstantX += moveSpeed
+                if controlsInverted:
+                    MovingConstantX -= moveSpeed
+                else:
+                    MovingConstantX += moveSpeed
 
             if events.key == pygame.K_a or events.key == pygame.K_LEFT:
-                MovingConstantX -= moveSpeed
+                if controlsInverted:
+                    MovingConstantX += moveSpeed
+                else:
+                    MovingConstantX -= moveSpeed
 
         if events.type == pygame.KEYUP:
             if events.key == pygame.K_w or events.key == pygame.K_s or events.key == pygame.K_UP or events.key == pygame.K_DOWN:
@@ -170,7 +250,7 @@ while runs:
         for x in spikelocs:
             if x.colliderect(player):
                 GameOver = True
-                GameOverScreen()
+                Saved = GameOverScreen(Saved)
 
     ches = renderCheseburger(cheseburgerloc[0], cheseburgerloc[1])
 
@@ -221,14 +301,18 @@ while runs:
     if GameOver != True:
         screen.blit(toiletstat, (300, 530))
         screen.blit(toiletwarning, (300, 550))
+    
+    if GameOver == True:
+        gameoverscreentitle = GFont_large.render("Game over.", False, (255,0,0))
+        screen.blit(gameoverscreentitle, (30, 300))
 
     if toiletMeter > 10:
         GameOver = True
-        GameOverScreen()
+        Saved = GameOverScreen(Saved)
 
     if random.randint(0, 999) == 69:
 
-        event = random.choice(["spikes", "rnggod", "rnggodfail", "speedup", "speeddown"])
+        event = random.choice(["spikes", "rnggod", "rnggodfail", "speedup", "speeddown", "invert"])
         if event == "spikes":
             spikes.append((random.randint(0, 800-64), random.randint(0, 600-64)))
             reaction = GFont_smaller.render(f"Event: Spike planted!", False, (255,255,255))
@@ -248,9 +332,18 @@ while runs:
             by = [0.1,0.2]
             moveSpeed -= random.choice(by)
             reaction = GFont_smaller.render(f"Event: Snail moment", False, (255,255,255))
+        elif event == "invert":
+            if controlsInverted:
+                reaction = GFont_smaller.render("Event: No more weird controls!", False, (255,255,255))
+                controlsInverted = False
+            else:
+                reaction = GFont_smaller.render("Event: Turn that frown. Controls inverted.", False, (255,255,255))
+                controlsInverted = True
         
     screen.blit(reaction, (400, 10))
     version = GFont_smaller.render(f"Build V0.1, RandomboiXD", False, (255,0,0))
+    records = GFont_smaller.render(f"High score: {str(record)}", False, (0,255,0))
+    screen.blit(records, (1,20))
     screen.blit(version, (1,1))
     pygame.display.update()
     dice = random.randint(0, toilettakechance)
