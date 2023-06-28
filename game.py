0b17806ec141e6b166c8daf76ba9efd97e6cdcf0
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2023-06-28 21:10:36 +0200

    Hamburger Menus everywhere update

--- game.py
+++ game.py
@@ -17,6 +17,7 @@ import random
 import base64
 import json
 import requests
+import time
 
 
 pygame.init()
@@ -35,18 +36,22 @@ THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR I
 
 
 def DownloadFiles(): # This function downloads assets from https://rndwebsite.ddns.net/ You can replace the url with your own, just make sure you return bytes.
-    if os.path.exists("./HamburgerAssets/") and os.path.exists(f"./HamburgerAssets/burgir.png") and os.path.exists("./HamburgerAssets/cheeseburger.png") and os.path.exists("./HamburgerAssets/Player.png") and os.path.exists("./HamburgerAssets/spike.png") and os.path.exists("./HamburgerAssets/toilet.png") and os.path.exists("./HamburgerAssets/shop.png") and os.path.exists("./HamburgerAssets/coin.png"):
+    if os.path.exists("./HamburgerAssets/") and os.path.exists(f"./HamburgerAssets/burgir.png") and os.path.exists("./HamburgerAssets/cheeseburger.png") and os.path.exists("./HamburgerAssets/Player.png") and os.path.exists("./HamburgerAssets/spike.png") and os.path.exists("./HamburgerAssets/toilet.png") and os.path.exists("./HamburgerAssets/shop.png") and os.path.exists("./HamburgerAssets/coin.png") and os.path.exists("./HamburgerAssets/gear.png") and os.path.exists("./HamburgerAssets/backarrow.png"):
         print("Game assets are present, Starting game!")
     else:
         os.mkdir("./HamburgerAssets/")
         print("Connecting to https://rndwebsite.ddns.net/ and downloading assets!")
         burgir = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/burgir")
         cheeseburger = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/cheeseburger")
+        time.sleep(2)
         Player = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/Player")
         Spike = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/spike")
         toilet = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/toilet")
         Coin = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/coin")
+        time.sleep(2)
         Shop = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/shop")
+        gear = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/gear")
+        backarrow = requests.get("https://rndwebsite.ddns.net/cdn/hamburgerexe/backarrow")
 
         with open(f"./HamburgerAssets/burgir.png", "xb") as f:
             f.write(burgir.content)
@@ -68,6 +73,12 @@ def DownloadFiles(): # This function downloads assets from https://rndwebsite.dd
         
         with open(f"./HamburgerAssets/shop.png", "xb") as f:
             f.write(Shop.content)
+        
+        with open(f"./HamburgerAssets/gear.png", "xb") as f:
+            f.write(gear.content)
+
+        with open(f"./HamburgerAssets/backarrow.png", "xb") as f:
+            f.write(backarrow.content)
 
         print("Finished downloading (and writing) files! Starting game!")
 
@@ -77,7 +88,9 @@ print(licensetext) # Print the MIT License
 screen = pygame.display.set_mode((800, 600)) # Set the display to 800,600
 # Import assets/sprites/whatever
 
-pygame.display.set_caption("./HamburgerAssets/Hamburger.exe")
+antialias = False
+
+pygame.display.set_caption("Hamburger.exe - Main Menu")
 spikeSprite = pygame.image.load("./HamburgerAssets/spike.png")
 logo = pygame.image.load("./HamburgerAssets/burgir.png")
 toiletSprite = pygame.image.load("./HamburgerAssets/toilet.png")
@@ -85,6 +98,8 @@ cheseburgerSprite = pygame.image.load("./HamburgerAssets/cheeseburger.png")
 PlayerIcon = pygame.image.load("./HamburgerAssets/Player.png")
 CoinSprite = pygame.image.load("./HamburgerAssets/coin.png")
 ShopSprite = pygame.image.load("./HamburgerAssets/shop.png")
+OptionsSprite = pygame.image.load("./HamburgerAssets/gear.png")
+BackArrowSprite = pygame.image.load("./HamburgerAssets/backarrow.png")
 pygame.display.set_icon(logo)
 del logo
 
@@ -131,7 +146,13 @@ hasbackround = True
 Coins = 0
 
 
-reaction = GFont_smaller.render("Event: - nothing -", False, (255,255,255))
+
+Randomtitles = ["Now with annoying flies!", "Welcome to the shoppin' channel!", "Are you H A M B U R G E R enough?"]
+
+randomtitle = random.choice(Randomtitles)
+
+
+reaction = GFont_smaller.render("Event: - nothing -", antialias, (255,255,255))
 
 if os.path.exists("hamburger.records"): # If possible, load the records
     with open("hamburger.records", "r") as f:
@@ -218,6 +239,148 @@ def renderCoins():
         iteration += 1
     return [c, locations]
 
+
+def ConfirmationMenu(message):
+    confmenu = True
+    global antialias
+    while confmenu:
+        screen.fill((0,179,119))
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                QuitMenu()
+            
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_ESCAPE:
+                    QuitMenu()
+                
+                if events.key == pygame.K_RETURN:
+                    confmenu = False
+
+        confmsg = GFont_smaller.render(message, antialias, (255,255,255))
+        successmsg = GFont.render("Success", antialias, (255,255,255))
+        confirm = GFont_smaller.render("Hit enter to go back", antialias, (255,255,255))
+        screen.blit(confmsg, (300, 200))
+        screen.blit(successmsg, (330, 10))
+        screen.blit(confirm, (300, 500))
+
+        pygame.display.update()
+
+    return
+
+def QuitMenu():
+    quitmenu = True
+    global antialias
+    while quitmenu:
+        screen.fill((128,0,0))
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                pygame.quit()
+                quit(0)
+            
+            if events.type == pygame.KEYDOWN:
+
+                if events.key == pygame.K_ESCAPE:
+                    quitmenu = False
+
+                if events.key == pygame.K_RETURN:
+                    pygame.quit()
+                    quit(0)
+        
+        leaving = GFont.render("Leaving hamburger.exe?", antialias, (255,255,255))
+        leavingconfirm = GFont_smaller.render("Hit enter to quit, hit ESC to go back", antialias, (255,255,255))
+        screen.blit(leaving, (230, 10))
+        screen.blit(leavingconfirm, (240, 500))
+        pygame.display.set_caption("Hamburger.exe - Quitting")
+        pygame.display.update()
+    return
+
+
+def OptionsMenu():
+    optionsmenu = True
+    while optionsmenu:
+        global antialias
+        global record
+        screen.fill((26,153,0))
+        optionstitle = GFont.render("Options", antialias, (255,255,255))
+        for events in pygame.event.get():
+            if events.type == pygame.QUIT:
+                QuitMenu()
+            
+            if events.type == pygame.KEYDOWN:
+                if events.key == pygame.K_q:
+                    optionsmenu = False
+
+                if events.key == pygame.K_ESCAPE:
+                    QuitMenu()
+
+                if events.key == pygame.K_a:
+                    if antialias == True:
+                        antialias = False
+                    else:
+                        antialias = True
+                    ConfirmationMenu("Anti alias toggled!")
+
+                if events.key == pygame.K_z:
+                    if os.path.exists("./hamburger.records"):
+                        os.remove("hamburger.records")
+                        record = 0
+                        ConfirmationMenu("Successfully deleted records!")
+
+        if antialias == True:
+            antialias_setting = GFont_smaller.render("[toggle with A] Text anti alias (Experimental): ON", antialias, (255,166,77))
+        else:
+            antialias_setting = GFont_smaller.render("[toggle with A] Text anti alias (Experimental): OFF", antialias, (255,166,77))
+
+        record_delete = GFont_smaller.render("[delete with Z] Delete saved records (Warning: Permanent!)", antialias, (255,166,77))
+
+
+        backarrowhint = GFont_smaller.render("Hit Q to go back", antialias, (255,255,255))
+
+        screen.blit(optionstitle, (350, 10))
+        screen.blit(antialias_setting, (100, 50))
+        screen.blit(record_delete, (100, 80))
+        screen.blit(BackArrowSprite, (30, 500))
+        screen.blit(backarrowhint, (10, 560))
+
+        pygame.display.set_caption("Hamburger.exe - Options")
+        pygame.display.update()
+    return
+
+Menu = True
+while Menu:
+    screen.fill((102,25,255))
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            QuitMenu()
+        
+        if events.type == pygame.KEYDOWN:
+            if events.key == pygame.K_RETURN:
+                Menu = False
+            
+            if events.key == pygame.K_ESCAPE:
+                QuitMenu()
+
+            if events.key == pygame.K_q:
+                OptionsMenu()
+
+    ttl = GFont_large.render("Hamburger.exe", antialias, (230,115,0))
+    randomtitletext = GFont_smaller.render(randomtitle, antialias, (230,230,0))
+    hiscore = GFont_smaller.render(f"Your highscore is: {str(record)}", False,(26,153,0))
+    hitentertocontinue = GFont_smaller.render("Hit enter to start game", False, (0,26,21))
+    copyrighttext = GFont_smaller.render("hamburger.exe v0.3 - By RandomboiXD (2023)", False, (255, 0,0))
+    copyrighttext2 = GFont_smaller.render("Licensed Under MIT, read console for license text", False, (255,0,0))
+    optionshint = GFont_smaller.render("Hit Q for settings", False, (255, 255 ,255))
+    screen.blit(ttl, (240, 150))
+    screen.blit(randomtitletext, (245, 200))
+    screen.blit(hiscore, (245, 250))
+    screen.blit(hitentertocontinue, (280, 500))
+    screen.blit(copyrighttext, (1,1))
+    screen.blit(copyrighttext2, (1, 20))
+    screen.blit(OptionsSprite, (700-32, 500))
+    screen.blit(optionshint, (625, 560))
+    pygame.display.set_caption("Hamburger.exe - Main Menu")
+    pygame.display.update()
+
 runs = True
 while runs:
     if hasbackround == True:
@@ -226,9 +389,7 @@ while runs:
     for events in pygame.event.get():
 
         if events.type == pygame.QUIT:
-            runs = False
-            print("Exiting")
-            break
+            QuitMenu()
         
         if events.type == pygame.KEYDOWN:
             
@@ -277,9 +438,7 @@ while runs:
 
         if events.type == pygame.KEYDOWN:
             if events.key == pygame.K_ESCAPE:
-                runs = False
-                print("Exiting")
-                break
+                QuitMenu()
 
             if events.key == pygame.K_LALT:
                 print("--- Start Variable Dump ---")
@@ -297,11 +456,6 @@ while runs:
             if events.key == pygame.K_g:
                 hasbackround = True
 
-                    
-
-        
-
-        
 
         if events.type == pygame.KEYDOWN and events.key == pygame.K_e and ches.colliderect(player) and GameOver != True:
             cheseburgerloc = (random.randint(200, 500), random.randint(100, 500))
@@ -331,7 +485,7 @@ while runs:
         player = renderPlayer(99999999,9999999)
 
     tutorialscreen()
-    CoinSign = GFont_smaller.render(f"You have {str(Coins)} coins!", False, (255,255,255))
+    CoinSign = GFont_smaller.render(f"You have {str(Coins)} coins!", antialias, (255,255,255))
     screen.blit(CoinSign, (620, 580))
     spikelocs = renderSpikes()
     if spikelocs == []:
@@ -349,7 +503,10 @@ while runs:
         for x,y in zip(coinslocations[0], coinslocations[1]):
             if x.colliderect(player):
                 Coins += 1
-                del coinslocs[y]
+                try:
+                    del coinslocs[y]
+                except:
+                    print(f"An error occured when trying to delete key {str(y)} of coins")
                 del x
 
     ches = renderCheseburger(cheseburgerloc[0], cheseburgerloc[1])
@@ -386,28 +543,28 @@ while runs:
 
     toilet = renderToilet(ToiletX, ToiletY)
     if ches.colliderect(player):
-        sup = GFont_smaller.render("Press E to pick up cheseburger!", False, (255,255,255))
+        sup = GFont_smaller.render("Press E to pick up cheseburger!", antialias, (255,255,255))
         screen.blit(sup, fontbottom)
 
     if toilet.colliderect(player) and toiletMeter > 8:
-        sup = GFont_smaller.render("Press E to take a dump", False, (255,255,255))
+        sup = GFont_smaller.render("Press E to take a dump", antialias, (255,255,255))
         screen.blit(sup, fontbottom)
 
-    stat = GFont_smaller.render(f"Hamburgers eaten: {str(cheseburgerstouched)}", False, (255,255,255))
+    stat = GFont_smaller.render(f"Hamburgers eaten: {str(cheseburgerstouched)}", antialias, (255,255,255))
     screen.blit(stat, burgerstat)
     pygame.display.set_caption(f"Hamburger.exe - {str(cheseburgerstouched)} chesburgers eaten")
 
     if toiletMeter > 8:
-        toiletstat = GFont_smaller.render(f"GO TO THE TOILET!", False, (255,255,255))
+        toiletstat = GFont_smaller.render(f"GO TO THE TOILET!", antialias, (255,255,255))
     else:
-        toiletstat = GFont_smaller.render(f"Your toilet meter: {str(toiletMeter)}/10", False, (255,255,255))
-    toiletwarning = GFont_smaller.render("Going over 10 is a game over!", False, (255,0,0))
+        toiletstat = GFont_smaller.render(f"Your toilet meter: {str(toiletMeter)}/10", antialias, (255,255,255))
+    toiletwarning = GFont_smaller.render("Going over 10 is a game over!", antialias, (255,0,0))
     if GameOver != True:
         screen.blit(toiletstat, (300, 530))
         screen.blit(toiletwarning, (300, 550))
     
     if GameOver == True:
-        gameoverscreentitle = GFont_large.render("Game over.", False, (255,0,0))
+        gameoverscreentitle = GFont_large.render("Game over.", antialias, (255,0,0))
         screen.blit(gameoverscreentitle, (30, 300))
 
     if toiletMeter > 10:
@@ -419,56 +576,56 @@ while runs:
         event = random.choice(["spikes", "rnggod", "rnggodfail", "speedup", "speeddown", "invert", "nobackround", "randombackround", "jackpot"])
         if event == "spikes":
             spikes.append((random.randint(0, 800-64), random.randint(0, 600-64)))
-            reaction = GFont_smaller.render(f"Event: Spike planted!", False, (255,255,255))
+            reaction = GFont_smaller.render(f"Event: Spike planted!", antialias, (255,255,255))
         elif event == "rnggod":
             ups = random.randint(0, 200)
             toilettakechance += ups
-            reaction = GFont_smaller.render(f"Event: RNG Gods like you", False, (255,255,255))
+            reaction = GFont_smaller.render(f"Event: RNG Gods like you", antialias, (255,255,255))
         elif event == "rnggodfail":
             downs = random.randint(0, 200)
             toilettakechance -= downs
-            reaction = GFont_smaller.render(f"Event: The RNG Gods hate you", False, (255,255,255))
+            reaction = GFont_smaller.render(f"Event: The RNG Gods hate you", antialias, (255,255,255))
         elif event == "speedup":
             by = [0.1,0.2,0.3,0.4,0.5,0.6]
             moveSpeed += random.choice(by)
-            reaction = GFont_smaller.render(f"Event: Im sanic! Sanic the hedgehog!", False, (255,255,255))
+            reaction = GFont_smaller.render(f"Event: Im sanic! Sanic the hedgehog!", antialias, (255,255,255))
         elif event == "speeddown":
             by = [0.1,0.2]
             moveSpeed -= random.choice(by)
-            reaction = GFont_smaller.render(f"Event: Snail moment", False, (255,255,255))
+            reaction = GFont_smaller.render(f"Event: Snail moment", antialias, (255,255,255))
         elif event == "invert":
             if controlsInverted:
-                reaction = GFont_smaller.render("Event: No more weird controls!", False, (255,255,255))
+                reaction = GFont_smaller.render("Event: No more weird controls!", antialias, (255,255,255))
                 controlsInverted = False
             else:
-                reaction = GFont_smaller.render("Event: You know what? *inverts your controls*", False, (255,255,255))
+                reaction = GFont_smaller.render("Event: You know what? *inverts your controls*", antialias, (255,255,255))
                 controlsInverted = True
         elif event == "nobackround":
             if hasbackround:
                 hasbackround = False
-                reaction = GFont_smaller.render("Event: Eh who needs a backround?", False, (255,255,255))
+                reaction = GFont_smaller.render("Event: Eh who needs a backround?", antialias, (255,255,255))
                 print("Backround was disabled! Hit G to fix!")
             else:
                 hasbackround = True
-                reaction = GFont_smaller.render("Event: yo we need that backround", False, (255,255,255))
+                reaction = GFont_smaller.render("Event: yo we need that backround", antialias, (255,255,255))
         elif event == "randombackround":
             backroundcolor = random.choice(colorcodes)
-            reaction = GFont_smaller.render("Event: New color time!", False, (255,255,255))
+            reaction = GFont_smaller.render("Event: New color time!", antialias, (255,255,255))
         elif event == "jackpot":
             howmuch = random.randint(1, 100)
             for x in range(howmuch):
                 coinslocs.append((random.randint(100, 700), random.randint(100, 500)))
-            reaction = GFont_smaller.render("Event: WOOOOO Jackpot!", False, (255,255,255))
+            reaction = GFont_smaller.render("Event: WOOOOO Jackpot!", antialias, (255,255,255))
         
     screen.blit(reaction, (400, 10))
-    version = GFont_smaller.render(f"Build V0.2, RandomboiXD", False, (255,0,0))
-    records = GFont_smaller.render(f"High score: {str(record)}", False, (0,255,0))
+    version = GFont_smaller.render(f"Build V0.3, RandomboiXD", antialias, (255,0,0))
+    records = GFont_smaller.render(f"High score: {str(record)}", antialias, (0,255,0))
     screen.blit(records, (1,20))
     screen.blit(version, (1,1))
     if shop.colliderect(player):
-        HintDisplay = GFont_smaller.render("Welcome to the shop!", False, (255,255,255))
-        HintDisplay2 = GFont_smaller.render("X = -1 Speed,C = + 1 speed, 3 coins each!", False, (255,255,255))
-        HintDisplay3 = GFont_smaller.render("B = No damage from spikes (20 coins)", False, (255,255,255))
+        HintDisplay = GFont_smaller.render("Welcome to the shop!", antialias, (255,255,255))
+        HintDisplay2 = GFont_smaller.render("X = -1 Speed,C = + 1 speed, 3 coins each!", antialias, (255,255,255))
+        HintDisplay3 = GFont_smaller.render("B = No damage from spikes (20 coins)", antialias, (255,255,255))
         screen.blit(HintDisplay, (PlayerX, PlayerY-30))
         screen.blit(HintDisplay2, (PlayerX, PlayerY-50))
         screen.blit(HintDisplay3, (PlayerX, PlayerY-70))
